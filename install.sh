03a94b5c014ed60d92f75b73efe34d3ba4d6e7cd
Kyle Finley <kyle@kylefinley.net>
2021-09-21 13:31:35 -0700

    Updated VS extension install setup and install script.

--- install.sh
+++ install.sh
@@ -1,26 +1,57 @@
 #!/usr/bin/env sh
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Installing Home brew..."
+    # Install Homebrew
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    brew update
+fi
 
 echo "Installing packages..."
-brew install --cask iterm2
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Cloning zsh plugins"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ ! -d /Applications/iTerm.app ]; then
+    # proceed with installing iTerm
+    brew install --cask iterm2
+fi
+
+if [ ! -d ~/.oh-my-zsh ]; then
+	echo "Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+    echo "Cloning zsh plugins"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
 echo "Linking dotfiles..."
 
+if [ -f ~/.zshrc ]; then
+    mv ~/.zshrc ~/.zshrc_backup
+fi 
+
 # shell settings
 ln -sv $HOME/.dotfiles/zsh/.zshrc ~/.zshrc
+
+if [ -f ~/.gitconfig ]; then
+    mv ~/.gitconfig ~/.gitconfig_backup
+fi 
 
 # git configurations
 ln -sv $HOME/.dotfiles/git/.gitconfig ~/.gitconfig
 
 # vscode settings
+if [ -f $HOME/Library/Application\ Support/Code/User/settings.json ]; then
+    mv $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings_backup.json
+fi 
 ln -sv $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-ln -sv $HOME/.dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-ln -sv $HOME/.dotfiles/vscode/snippets/ $HOME/Library/Application\ Support/Code/User
 
-cat ~/.dotfiles/vscode/extensions | while read extension
-do
-    code --install-extension $extension --force
-done
+if [ -f $HOME/Library/Application\ Support/Code/User/keybindings.json ]; then
+    mv $HOME/Library/Application\ Support/Code/User/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings_backup.json
+fi 
+ln -sv $HOME/.dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+
+source ~/.dotfiles/system/.path
+
+source ~/.dotfiles/vscode/install-extensions.sh
